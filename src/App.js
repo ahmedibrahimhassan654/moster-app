f950dc402c3e6364c89f2b51f68a8e76dbdeb549
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-01-23 22:02:55 +0200

    47. Monsters Rolodex - Component Props

--- src/App.js
+++ src/App.js
@@ -23,45 +23,40 @@ class App extends Component {
   }
 
   onsearchCahnge = (e) => {
-    console.log("event.target.value", e.target.value);
-    console.log({ startingArray: this.state.monsters });
     const searchedString = e.target.value.toLocaleLowerCase();
 
-    this.setState(
-      () => {
-        return { searchedString };
-      },
-      () => {
-        console.log({ endingArray: this.state.monsters });
-      }
-    );
+    this.setState(() => {
+      return { searchedString };
+    });
   };
   render() {
-    console.log("start rendering ");
     const { monsters, searchedString } = this.state;
     const { onsearchCahnge } = this;
     const filteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchedString);
     });
-    console.log("render", filteredMonsters);
-    return (
-      <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="search monsters"
-          onChange={onsearchCahnge}
-        />
 
-        {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <p>{monster.name}</p>;
-            </div>
-          );
-        })}
-        <CardListComp />
-      </div>
+    return (
+      <>
+        <div className="App">
+          <input
+            className="search-box"
+            type="search"
+            placeholder="search monsters"
+            onChange={onsearchCahnge}
+          />
+          {/* 
+          {filteredMonsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <p>{monster.name}</p>;
+              </div>
+            );
+          })}
+          */}
+          <CardListComp monsters={filteredMonsters} />
+        </div>
+      </>
     );
   }
 }
--- src/components/cardList/cardListComp.jsx
+++ src/components/cardList/cardListComp.jsx
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 class CardListComp extends Component {
   render() {
-    return (
-      <div>
-        <p>Hello i'm the card list comp'</p>
-      </div>
-    );
+    console.log("get all monsters from inside card list comp ", this.props);
+    const { monsters } = this.props;
+    return monsters.map((monster) => {
+      return <h1 key={monster.id}>{monster.name}</h1>;
+    });
   }
 }
 export default CardListComp;
